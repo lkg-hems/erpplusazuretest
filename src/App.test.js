97f50c4ef3a245e385e80f76bc2be93b814276cb
hems <hems@linkstec.com>
2023-05-17 14:55:54 +0900

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React For ERPPlus(Claudio様向け)/i);
   expect(linkElement).toBeInTheDocument();
 });
